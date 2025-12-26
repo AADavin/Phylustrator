1054fe589b70c8bf7668f8eb4ce91621cb509bac
Adrián A. Davín <35602649+AADavin@users.noreply.github.com>
2025-12-26 17:18:52 +0100

    refactoring

--- src/phylustrator/__init__.py
+++ src/phylustrator/__init__.py
@@ -1,6 +1,20 @@
 from .drawing import TreeStyle, BaseDrawer, RadialTreeDrawer, VerticalTreeDrawer
-from .ale_parser import parse_ale_file, parse_uts_file, ALEData
-from .alerax_parser import parse_alerax, AleRaxData
-from .zombi_parser import ZombiParser, ZombiData, parse_zombi
-from .utils import add_origin_if_root_has_dist
+from .parsers import (
+    # Alerax
+    parse_alerax, 
+    AleRaxData,
+    
+    # Zombi
+    ZombiParser, 
+    ZombiData, 
+    parse_zombi,
+    
+    # ALE
+    ALEParser, 
+    ALEData, 
+    parse_uts_file, 
+    parse_ale_file, 
+    parse_ale_folder,
 
+    add_origin_if_root_has_dist
+)
