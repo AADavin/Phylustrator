6817537325bb4f31076c2eb503282c6e6e78e029
Adrián A. Davín <35602649+AADavin@users.noreply.github.com>
2026-01-22 12:13:00 +0100

    cleaning extra files

--- src/phylustrator/__init__.py
+++ src/phylustrator/__init__.py
@@ -1,19 +1,1 @@
 from .drawing import TreeStyle, BaseDrawer, RadialTreeDrawer, VerticalTreeDrawer
-from .parsers import (
-    # Alerax
-    parse_alerax, 
-    AleRaxData,
-    
-    # Zombi
-    ZombiParser, 
-    ZombiData, 
-    parse_zombi,
-    
-    # ALE
-    ALEData, 
-    parse_uts_file, 
-    parse_ale_file, 
-    parse_ale_folder,
-
-    add_origin_if_root_has_dist
-)
--- src/phylustrator/parsers/__init__.py
+++
@@ -1,4 +0,0 @@
-from .alerax_parser import parse_alerax, AleRaxData
-from .zombi_parser import ZombiParser, ZombiData, parse_zombi
-from .ale_parser import ALEData, parse_uts_file, parse_ale_file, parse_ale_folder
-from ..utils import add_origin_if_root_has_dist
